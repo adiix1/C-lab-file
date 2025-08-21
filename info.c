8c4d1c75ccaa41df9eaf0ec55bfa9ed2ab0aa4e9
adiix1 <adiisiingh7500@gmail.com>
2025-08-21 22:40:03 +0530

    Update info.c

--- info.c
+++ info.c
@@ -1,8 +1,8 @@
 # include <stdio.h>
 
 int main() {
-    printf("Name :- Nirbhay Garg\n");
-    printf("Sap ID :- 590022827\n");
-    printf("email :- nirbhaygarg11@gmail.com\n");
+    printf("Name :- Aditya Singh\n");
+    printf("Sap ID :- 590022053\n");
+    printf("email :- adiisiingh7500@gmail.com\n");
     return 0;
 }
